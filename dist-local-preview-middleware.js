7907a9da0dade15ec4a736321635013ced559e17
wancheng1990 <25022549@qq.com>
2021-03-31 10:48:36 +0800

    init files

--- dist-local-preview-middleware.js
+++ dist-local-preview-middleware.js
@@ -1,68 +1,31 @@
-# dist-local-preview-middleware
- dist local preview middleware
-
-
-# 背景
-
-由于`http-proxy` 不支持 `pathRewrite` 参数，所以没得办法只能通过 `http-proxy-middleware`插件来实现。
-
-
-项目所依赖的
-
-- connect
-- serve-static
-- http-proxy-middleware
-- http
-- open
-
-
-
-引用依赖
-```
 const connect = require('connect');
 const serveStatic = require('serve-static');
 const http = require('http');
 const {createProxyMiddleware} = require('http-proxy-middleware');
-```
 
-
-基本配置
-
-```
+const app = connect();
 const config = {
-    target: 'http://xx.xx.xx.xx',
+    target: 'http://localhost:9093',
     port: 3001,
     host: '0.0.0.0',
     dir: '../dist',
     prefix: '/api',
     publicPath: '/'
 };
-```
 
-http-proxy-middleware 配置
-
-```
 const proxyConfigMiddleware = {
     target: config.target,
     changeOrigin: true,
     logLevel: 'debug',
-    // path rewrite 
     pathRewrite: {
         ['^' + config.prefix]: ''
     }
 };
-```
 
-
-利用 `createProxyMiddleware` 方法创建 proxyMiddleware
-```
 const proxyMiddleware = createProxyMiddleware(proxyConfigMiddleware);
 
-```
-
-配置本地的http server
-```
 app.use(config.prefix, proxyMiddleware);
+
 app.use(
     config.publicPath,
     serveStatic(config.dir, {
@@ -70,15 +33,7 @@ app.use(
     })
 );
 
+
 http.createServer(app).listen(config.port, config.host);
-```
 
-
-调用 `open` 打开默认浏览器
-```
 require('open')('http://localhost:' + config.port);
-```
-
-
-
-
